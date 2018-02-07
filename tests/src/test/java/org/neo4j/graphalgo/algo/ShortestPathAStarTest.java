747272ebcd947e9930a4c10c2a6a56b3199ff5ba
Sadeep Madurange <sadeepmadurange@gmail.com>
2018-02-07 17:26:32 +0800

    Added unit test to result the nodes and distance result.

--- tests/src/test/java/org/neo4j/graphalgo/algo/ShortestPathAStarTest.java
+++ tests/src/test/java/org/neo4j/graphalgo/algo/ShortestPathAStarTest.java
@@ -1,14 +1,17 @@
 package org.neo4j.graphalgo.algo;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertArrayEquals;
 
-import java.util.function.DoubleConsumer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.neo4j.graphalgo.ShortestPathProc;
 import org.neo4j.graphalgo.TestDatabaseCreator;
+import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.api.exceptions.KernelException;
 import org.neo4j.kernel.impl.proc.Procedures;
@@ -26,7 +29,7 @@ public class ShortestPathAStarTest {
 		 * Distance = 2979 NM
 		 * */
         String createGraph =
-                "CREATE (nA:Node{name:'SINGAPORE', latitude:1.304444,longitude:103.717373})\n" + // start
+                "CREATE (nA:Node{name:'SINGAPORE', latitude:1.304444,longitude:103.717373})\n" +
                         "CREATE (nB:Node{name:'SINGAPORE STRAIT', latitude:1.1892, longitude:103.4689})\n" +
                         "CREATE (nC:Node{name:'WAYPOINT 68', latitude:8.83055556, longitude:111.8725})\n" +
                         "CREATE (nD:Node{name:'WAYPOINT 70', latitude:10.82916667, longitude:113.9722222})\n" +
@@ -34,7 +37,15 @@ public class ShortestPathAStarTest {
                         "CREATE (nF:Node{name:'SOUTH CHINA SEA', latitude:16.0728, longitude:119.6128})\n" +
                         "CREATE (nG:Node{name:'LUZON STRAIT', latitude:20.5325, longitude:121.845})\n" +
                         "CREATE (nH:Node{name:'WAYPOINT 87', latitude:29.32611111, longitude:131.2988889})\n" +
-                        "CREATE (nX:Node{name:'CHIBA', latitude:35.562222, longitude:140.059187})\n" + // end
+                        "CREATE (nI:Node{name:'KARIMATA STRAIT', latitude:-2.0428, longitude:108.6225})\n" +
+                        "CREATE (nJ:Node{name:'LOMBOK STRAIT', latitude:-8.3256, longitude:115.8872})\n" +
+                        "CREATE (nK:Node{name:'SUMBAWA STRAIT', latitude:-8.5945, longitude:116.6867})\n" +
+                        "CREATE (nL:Node{name:'KOLANA AREA', latitude:-8.2211, longitude:125.2411})\n" +
+                        "CREATE (nM:Node{name:'EAST MANGOLE', latitude:-1.8558, longitude:126.5572})\n" +
+                        "CREATE (nN:Node{name:'WAYPOINT 88', latitude:3.96861111, longitude:128.3052778})\n" +
+                        "CREATE (nO:Node{name:'WAYPOINT 89', latitude:12.76305556, longitude:131.2980556})\n" +
+                        "CREATE (nP:Node{name:'WAYPOINT 90', latitude:22.32027778, longitude:134.700000})\n" +
+                        "CREATE (nX:Node{name:'CHIBA', latitude:35.562222, longitude:140.059187})\n" +
                         "CREATE\n" +
                         "  (nA)-[:TYPE {cost:29.0}]->(nB),\n" +
                         "  (nB)-[:TYPE {cost:694.0}]->(nC),\n" +
@@ -43,7 +54,16 @@ public class ShortestPathAStarTest {
                         "  (nE)-[:TYPE {cost:357.0}]->(nF),\n" +
                         "  (nF)-[:TYPE {cost:299.0}]->(nG),\n" +
                         "  (nG)-[:TYPE {cost:740.0}]->(nH),\n" +
-                        "  (nH)-[:TYPE {cost:587.0}]->(nX)";
+                        "  (nH)-[:TYPE {cost:587.0}]->(nX),\n" +
+                        "  (nB)-[:TYPE {cost:389.0}]->(nI),\n" +
+                        "  (nI)-[:TYPE {cost:584.0}]->(nJ),\n" +
+                        "  (nJ)-[:TYPE {cost:82.0}]->(nK),\n" +
+                        "  (nK)-[:TYPE {cost:528.0}]->(nL),\n" +
+                        "  (nL)-[:TYPE {cost:391.0}]->(nM),\n" +
+                        "  (nM)-[:TYPE {cost:364.0}]->(nN),\n" +
+                        "  (nN)-[:TYPE {cost:554.0}]->(nO),\n" +
+                        "  (nO)-[:TYPE {cost:603.0}]->(nP),\n" +
+                        "  (nP)-[:TYPE {cost:847.0}]->(nX)";
 
         db = TestDatabaseCreator.createTestDatabase();
         try (Transaction tx = db.beginTx()) {
@@ -62,21 +82,28 @@ public class ShortestPathAStarTest {
     }
 	
 	@Test
-    public void testAStarData() throws Exception {
-		final DoubleConsumer consumer = mock(DoubleConsumer.class);
+    public void testAStarResult() throws Exception {
+		List<String> expectedNode = Arrays.asList("SINGAPORE", "SINGAPORE STRAIT", "WAYPOINT 68", 
+				"WAYPOINT 70", "WAYPOINT 74", "SOUTH CHINA SEA", "LUZON STRAIT", "WAYPOINT 87", "CHIBA");
+		List<Double> expectedDistance = Arrays.asList(0.0, 29.0, 723.0, 895.0, 996.0, 1353.0, 
+				1652.0, 2392.0, 2979.0);
+		List<String> actualNode = new ArrayList<String>();
+		List<Double> actualDistance = new ArrayList<Double>(); 
         db.execute(
                 "MATCH (start:Node{name:'SINGAPORE'}), (end:Node{name:'CHIBA'}) " +
                         "CALL algo.shortestPath.astar(start, end, 'cost') " +
                         "YIELD nodeId, cost RETURN nodeId, cost ")
                 .accept(row -> {
                     long nodeId = row.getNumber("nodeId").longValue();
+                    Node node = db.getNodeById(nodeId);
+                    String nodeName = (String) node.getProperty("name");
                     double distance = row.getNumber("cost").doubleValue();
-                    consumer.accept(distance);
-                    System.out.printf("%d:%.1f, ",
-                            nodeId,
-                            distance);
+                    actualNode.add(nodeName);
+                    actualDistance.add(distance);
                     return true;
                 });
+        assertArrayEquals(expectedNode.toArray(), actualNode.toArray());
+        assertArrayEquals(expectedDistance.toArray(), actualDistance.toArray());
     }
         
 }
